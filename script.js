b18f083290779b11b66626e124f87c372c26ef0d
Kraven0112 <sushilChaudhary2059@gmail.com>
2024-08-07 15:43:31 +0530

    add user object

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ import axios from 'axios'
 axios.get('https://restcountries.com/v3.1/all').then((response)=>{
     console.log(response.data[0])
 })
+const user = {
+    firstName:"Sushil",
+    lastName:"Chaudhary",
+    age:21
+}
+console.log(user)
